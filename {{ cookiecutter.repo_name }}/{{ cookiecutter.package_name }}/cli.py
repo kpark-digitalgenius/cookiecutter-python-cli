0733c2a9d6f81ae1b6414575f29f5f5273efbed9
Kevin Park <kevin@digitalgenius.com>
2018-08-24 12:15:53 +0100

    corrected option name

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.package_name }}/cli.py
@@ -2,9 +2,9 @@ import click
 
 
 @click.command()
-@click.option('--as-robot', '-r', is_flag=True, help='Greet as a robot.')
+@click.option('--as_robot', '-r', is_flag=True, help='Greet as a robot.')
 @click.argument('name', default='world', required=False)
-def main(name, as_cowboy):
+def main(name, as_robot):
     """{{ cookiecutter.project_short_description }}"""
     greet = 'Beep boop, hello' if as_robot else 'Hello'
     click.echo('{0}, {1}.'.format(greet, name))
